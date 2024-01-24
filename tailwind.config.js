f19fdfbc9486cc6a9adc06685e8e976596b0ce92
jojupacr5 <joseph.paucar@gmail.com>
2024-01-23 23:07:48 -0500

    Agregando el config de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.php"],
+  theme: {
+    fontFamily: {
+      heebo: ["Heebo", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
