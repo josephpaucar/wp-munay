154108d088af82e5915f09d40589eb9d22129024
oscarpro <oscar.ch.r29@gmail.com>
2024-01-27 01:34:15 -0500

    dia 27 inicio munay

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php get_header(); ?>
 <h1 class="text-xl">Agregado por oscar </h1>
 <p class="text-xl">Agregado por Joseph </p>
+<p class="text-xl">Agregado por Joseph 2  </p>
 <?php get_footer(); ?>
+
