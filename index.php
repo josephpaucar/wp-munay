893123f94c140a022efdc3e88c150322bf9dfccc
jojupacr5 <joseph.paucar@gmail.com>
2024-01-27 01:55:54 -0500

    Se quito el parrafo de más

--- index.php
+++ index.php
@@ -2,11 +2,6 @@
 
 <h1 class="text-xl">Agregado por oscar </h1>
 <p class="text-xl">Agregado por Joseph </p>
-<<<<<<< HEAD
 
 <?php get_footer(); ?>
-=======
-<p class="text-xl">Agregado por Joseph 2  </p>
-<?php get_footer(); ?>
 
->>>>>>> origin/main
