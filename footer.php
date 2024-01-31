1cfe9448edd7a421b66099d3651b50b70c18f90d
jojupacr5 <joseph.paucar@gmail.com>
2024-01-31 00:06:41 -0500

    Se agregaron hooks de woocommerce al header y footer

--- footer.php
+++ footer.php
@@ -1,35 +1,42 @@
-<footer class="bg-black py-10 px-5">
-    <div class="container mx-auto flex flex-col md:flex-row items-center">
-        <div class="flex-none w-40 my-4 md:my-0">
-            <a href="<?php echo get_home_url( ); ?>">
-                <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/logo.png" alt="Logo" >
-            </a>
-        </div>
-        <div class="grow my-4 md:my-0">
-            <div class="munay_footer">
-            <?php 
-                wp_nav_menu( array(
-                    'theme_location' => 'footer_menu'
-                ));
-            ?>
-            </div>
-        </div>
-        <div class="flex-none w-40 my-4 md:my-0">
-            <div class="flex gap-4 justify-center md:justify-end">
-                <a href="https://www.instagram.com/jpaucar.vue/" target="_blank" class="duration-300 hover:scale-125 transition">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/fa_instagram.png" alt="Munay Instagram">
-                </a>
-                <a href="https://www.instagram.com/jpaucar.vue/" target="_blank" class="duration-300 hover:scale-125">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/fa_facebook.png" alt="Munay Facebook">
-                </a>
-                <a href="https://www.instagram.com/jpaucar.vue/" target="_blank" class="duration-300 hover:scale-125">
-                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/fa_twitter.png" alt="Munay Twitter">
+    </div><!-- .container -->
+</div><!-- #content -->
+
+    <?php do_action( 'storefront_before_footer' ); ?>
+
+    <footer class="bg-black py-10 px-5">
+        <div class="container mx-auto flex flex-col md:flex-row items-center">
+            <div class="flex-none w-40 my-4 md:my-0">
+                <a href="<?php echo get_home_url( ); ?>">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/logo.png" alt="Logo" >
                 </a>
             </div>
+            <div class="grow my-4 md:my-0">
+                <div class="munay_footer">
+                <?php 
+                    wp_nav_menu( array(
+                        'theme_location' => 'footer_menu'
+                    ));
+                ?>
+                </div>
+            </div>
+            <div class="flex-none w-40 my-4 md:my-0">
+                <div class="flex gap-4 justify-center md:justify-end">
+                    <a href="https://www.instagram.com/jpaucar.vue/" target="_blank" class="duration-300 hover:scale-125 transition">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/fa_instagram.png" alt="Munay Instagram">
+                    </a>
+                    <a href="https://www.instagram.com/jpaucar.vue/" target="_blank" class="duration-300 hover:scale-125">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/fa_facebook.png" alt="Munay Facebook">
+                    </a>
+                    <a href="https://www.instagram.com/jpaucar.vue/" target="_blank" class="duration-300 hover:scale-125">
+                        <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/fa_twitter.png" alt="Munay Twitter">
+                    </a>
+                </div>
+            </div>
         </div>
-    </div>
-    <p class="text-center text-white/70 mt-10 md:mt-0">Todos los derechos reservados 2024</p>
-</footer>
+        <p class="text-center text-white/70 mt-10 md:mt-0">Todos los derechos reservados 2024</p>
+    </footer>
+
+    <?php do_action( 'storefront_after_footer' ); ?>
 
 <?php wp_footer() ?>
 
--- header.php
+++ header.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <?php wp_head() ?>
 </head>
-<body>
+
+<body <?php body_class(); ?>>
+
+  <?php do_action( 'storefront_before_site' ); ?>
+
+  <?php do_action( 'storefront_before_header' ); ?>
+
+  <header class="container mx-auto">
+    <h2 class="text-xl text-center ">Munay Header</h2>
+  </header>
+
+  <?php	do_action( 'storefront_before_content' );	?>
+
+  <div id="content" class="site-content" tabindex="-1">
+		<div class="container mx-auto">
+
+		<?php
+		do_action( 'storefront_content_top' );
